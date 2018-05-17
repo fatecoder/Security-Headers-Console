9c4b8a708c658f94167a12958fdad1ac3feb5dde
alex <acalderon@dotnet.com.mx>
2018-05-17 10:40:45 -0500

    Features added to list

--- SecurityHeadersConsole.py
+++ SecurityHeadersConsole.py
@@ -4,23 +4,32 @@ import urllib2, socket
 from urlparse import urlparse
 
 class SecurityHeaderVerifier(object):
-	__security_headers = { "content-security-policy":   ["Not Found", ["connect-src", "default-src", "font-src", "frame-src", "img-src", "manifest-src", "media-src", "script-src", "style-src", "worker-src"]],
-						   "x-xss-protection":          ["Not Found", ["1","mode=block"]],
-						   "strict-transport-security": ["Not Found", ["max-age", "includeSubdomains"]],
-						   "x-frame-options":           ["Not Found", ["DENY", "SAMEORIGIN", "ALLOW-FROM"]],
-						   "public-keys-pins":          ["Not Found", ["pin-sha256","max-age", "report-uri"]],
-						   "x-content-type-options":    ["Not Found", ["nosniff"]] }
+	__security_headers = { "content-security-policy":   ["Not Found", ["default-src", "script-src", "connect-src", "img-src", "style-src"], 5, "Not Secure", "content-security-policy: default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self';"],
+						   "x-xss-protection":          ["Not Found", ["1", "mode", "block"], 3, "Not Secure", "x-ss-protection: 1; mode=block"],
+						   "strict-transport-security": ["Not Found", ["max-age", "includeSubDomains"], 2, "Not Secure", "strict-transport-security: max-age=YOUR MAX AGE; includeSubDomains"],
+						   "x-frame-options":           ["Not Found", ["DENY"], 1, "Not Secure", "x-frame-options: DENY"],
+						   "public-keys-pins":          ["Not Found", ["pin-sha256","max-age", "includeSubDomains", "report-uri"], 5, "Not Secure", "Public-Key-Pins: pin-sha256=PRIMARY_KEY; pin-sha256=BACKUP_KEY; max-age=PIN_CACHE_EXPIRE_TIME; includeSubdomains; report-uri=YOUR_SITE_TO_REPORT"],
+						   "x-content-type-options":    ["Not Found", ["nosniff"], 1, "Not Secure", "x-content-type-options: nosniff"] }
 
 	def check_headers(self, info):
 		for key in info:
 			if key in self.__security_headers:
+				accum = 0
+				self.__security_headers[key][0] = "Found"
 				for attrib in self.__security_headers[key][1]:
 					if attrib in info[key]:
-						self.__security_headers[key][0] = "Found"
+						accum += 1
+					if accum == self.__security_headers[key][2]:
+						self.__security_headers[key][3] = "Secure"
 
 	def show_verified_headers(self):
-		for header in self.__security_headers:
-			print "%s: %s" % (header, self.__security_headers[header][0])
+		for key in self.__security_headers:
+			print "%s: %s, %s" % (key, self.__security_headers[key][0], self.__security_headers[key][3])
+
+	def show_not_found_header_info(self):
+		for key in self.__security_headers:
+			if self.__security_headers[key][3] == "Not Secure":
+				print "FOR %s IT'S RECOMMENDED => %s" % (key, self.__security_headers[key][4])
 
 	def verify_url(self, url):
 		try:
@@ -35,6 +44,8 @@ class SecurityHeaderVerifier(object):
 			print "IP: %s\n\nSECURITY HEADERS" % ip
 			self.check_headers(info)
 			self.show_verified_headers()
+			print "--------------------------"
+			self.show_not_found_header_info()
 			print "\nRAW HEADERS\n%s" % info
 			return True
 
